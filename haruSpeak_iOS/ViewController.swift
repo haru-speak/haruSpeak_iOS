040d27353020f0ca50a830a1d807ceead9a41d74
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-01-11 20:11:07 +0900

    Initial Commit

---
+++ haruSpeak_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  haruSpeak_iOS
+//
+//  Created by 077tech on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
