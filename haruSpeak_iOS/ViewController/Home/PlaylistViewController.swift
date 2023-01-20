88c09e69b686b0b1116aedf95af90a2cd15b9290
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-01-20 13:10:20 +0900

    Feat: "Add Property and Subview for PlaylistViewController"

--- haruSpeak_iOS/ViewController/Home/PlaylistViewController.swift
+++ haruSpeak_iOS/ViewController/Home/PlaylistViewController.swift
@@ -12,6 +12,80 @@ import Then
 
 class PlaylistViewController: UIViewController{
     //MARK: - Properties
+// NAVIGATION BAR
+    let backButton = UIImageView().then{
+        $0.image = UIImage(named: "backbutton")?.withRenderingMode(.alwaysOriginal)
+    }
+    let reportButton = UIImageView().then{
+        $0.image = UIImage(named: "reportbutton")?.withRenderingMode(.alwaysOriginal)
+    }
+    let mainScrollView = UIScrollView()
+// TOPVIEW
+    let topView = UIView().then{
+        $0.backgroundColor = .black
+    }
+    let profileImage = UIImageView().then{
+        $0.backgroundColor = .lightGray
+    }
+    let nameLabel = UILabel().then{
+        $0.text = "무유"
+    }
+    let dateLabel = UILabel().then{
+        $0.text = "01/10 22:00"
+    }
+    let playlistTitle = UILabel().then{
+        $0.text = "Nothing beats \"About time\""
+    }
+    let lyricsScrollView = UIScrollView()
+    let lyricsView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    let lyricsLabel = UILabel().then{
+        $0.text = "My room is tiny. \n When I say tiny… I mean, really tiny. \nMy room is tiny.\nWhen I say tiny… I mean, really tiny.\nMy room is tiny.\n When I say tiny… I mean, really tiny. \nMy room is tiny.\nWhen I say tiny… I mean, really tiny.\nMy room is tiny.\n When I say tiny… I mean, really tiny. \nMy room is tiny.\nWhen I say tiny… I mean, really tiny.\nMy room is tiny.\n When I say tiny… I mean, really tiny. \nMy room is tiny.\nWhen I say tiny… I mean, really tiny.\nMy room is tiny."
+    }
+    let playlistProgressbar = UIProgressView()
+    let playtimeStart = UILabel().then{
+        $0.text = "00:00"
+    }
+    let playtimeEnd = UILabel().then{
+        $0.text = "00:19"
+    }
+    let commentImage = UIImageView().then{
+        $0.image = UIImage(named: "chatblue")?.withRenderingMode(.alwaysOriginal)
+    }
+    let commentCount = UILabel().then{
+        $0.text = "3"
+    }
+    let playButton = UIImageView().then{
+        $0.image = UIImage(named: "playbutton")?.withRenderingMode(.alwaysOriginal)
+    }
+    let heartImage = UIImageView().then{
+        $0.image = UIImage(named: "heart")?.withRenderingMode(.alwaysOriginal)
+    }
+    let heartCount = UILabel().then{
+        $0.text = "3"
+    }
+    
+//BOTTOMVIEW
+    let bottomVIew = UIView().then{
+        $0.backgroundColor = .yellow
+    }
+
+//COMMENTSUMBMIT
+    let bottomFixedView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    let commentSubmitView = UIView().then{
+        $0.backgroundColor = .lightGray
+    }
+    let commentTextField = UITextField().then{
+        $0.placeholder = "피드백을 남겨보세요!"
+    }
+    let submitButton = UIImageView().then{
+        $0.image = UIImage(named: "commentupload")?.withRenderingMode(.alwaysOriginal)
+    }
+    
+    
     
     //MARK: - LifeCycle
         override func viewDidLoad() {
@@ -29,6 +103,34 @@ class PlaylistViewController: UIViewController{
     
     //MARK: - addSubView
     func setupView(){
+        view.addSubview(self.backButton)
+        view.addSubview(self.reportButton)
+        view.addSubview(self.mainScrollView)
+        self.mainScrollView.addSubview(self.topView)
+        //TOPVIEW
+        self.topView.addSubview(self.profileImage)
+        self.topView.addSubview(self.nameLabel)
+        self.topView.addSubview(self.dateLabel)
+        self.topView.addSubview(self.playlistTitle)
+        self.topView.addSubview(self.lyricsScrollView)
+        self.lyricsScrollView.addSubview(self.lyricsView)
+        self.lyricsView.addSubview(self.lyricsLabel)
+        self.topView.addSubview(self.playlistProgressbar)
+        self.topView.addSubview(self.playtimeStart)
+        self.topView.addSubview(self.playtimeEnd)
+        self.topView.addSubview(self.commentImage)
+        self.topView.addSubview(self.commentCount)
+        self.topView.addSubview(self.playButton)
+        self.topView.addSubview(self.heartImage)
+        self.topView.addSubview(self.heartCount)
+        //BOTTOMVIEW
+        self.mainScrollView.addSubview(self.bottomVIew)
+        //COMMENTSUBMIT
+        view.addSubview(self.bottomFixedView)
+        self.bottomFixedView.addSubview(self.commentSubmitView)
+        self.commentSubmitView.addSubview(self.commentTextField)
+        self.commentSubmitView.addSubview(self.commentSubmitView)
+        
         
     }
     
