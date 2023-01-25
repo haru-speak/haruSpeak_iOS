6700076695cd1ed0c48ad393477118e9ce24a1b5
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-01-25 23:32:06 +0900

    Feat: "Add Second and Third ViewController"

---
+++ haruSpeak_iOS/ViewController/UserSettingViewController/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  haruSpeak_iOS
+//
+//  Created by 077tech on 2023/01/25.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class SecondViewController: UIViewController{
+    
+//MARK: - Properties
+   
+//MARK: - LifeCycle
+    override func viewDidLoad() {
+        setUpView()
+        layout()
+        addTarget()
+        
+    }
+//MARK: - AddSubview
+    func setUpView(){
+        
+    }
+
+//MARK: - Selector
+
+    
+    
+    
+//MARK: - Layout
+    func layout(){
+        
+    }
+    
+//MARK: - Target
+    func addTarget(){
+        
+    }
+}
---
+++ haruSpeak_iOS/ViewController/UserSettingViewController/ThirdViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ThirdViewController.swift
+//  haruSpeak_iOS
+//
+//  Created by 077tech on 2023/01/25.
+//
+import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class ThirdViewController: UIViewController{
+    
+//MARK: - Properties
+   
+//MARK: - LifeCycle
+    override func viewDidLoad() {
+        setUpView()
+        layout()
+        addTarget()
+        
+    }
+//MARK: - AddSubview
+    func setUpView(){
+        
+    }
+
+//MARK: - Selector
+
+    
+    
+    
+//MARK: - Layout
+    func layout(){
+        
+    }
+    
+//MARK: - Target
+    func addTarget(){
+        
+    }
+}
+
