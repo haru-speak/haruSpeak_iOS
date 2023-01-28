02cbc1bfc4278237c194fccee5fdcdf36c88a08f
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-01-28 14:08:32 +0900

    Fix: "bottom button layout"

--- haruSpeak_iOS/SceneDelegate.swift
+++ haruSpeak_iOS/SceneDelegate.swift
@@ -26,7 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         studyVC.tabBarItem.image = UIImage(systemName: "square.grid.2x2")
         studyVC.title = "스터디"
         
-        let myPageVC = UINavigationController(rootViewController:JoinMSViewController())
+        let myPageVC = UINavigationController(rootViewController:FifthViewController())
         myPageVC.tabBarItem.image = UIImage(systemName: "person.crop.circle")
         myPageVC.title = "마이페이지"
         
